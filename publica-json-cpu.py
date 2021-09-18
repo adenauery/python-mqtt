063db7647af0005322ae9577356f40eb8f9423b6
Adenauer Yamin <adenauer@gmail.com>
2021-09-18 10:48:47 -0300

    Update publica-json-cpu.py

--- publica-json-cpu.py
+++ publica-json-cpu.py
@@ -3,7 +3,7 @@ import psutil
 
 sensor_cpu = psutil.cpu_percent(interval=1)
 
-comando = "mosquitto_pub -h broker.emqx.io -t pi/dados  -m " + "'" + '{"id":  "cpu1", "origem":  "adenauer", "descricao":  "ocupacao cpu", "Valor": ' + '"' + str(sensor_cpu)
+comando = "mosquitto_pub -h broker.emqx.io -t pi/dados  -m " + "'" + '{"id":  "cpu1", "origem":  "adenauer", "descricao":  "ocupacao cpu", "valor": ' + '"' + str(sensor_cpu)
 comando2 = comando + '"}' + "'"
 #print(comando2)
 os.system(comando2)
