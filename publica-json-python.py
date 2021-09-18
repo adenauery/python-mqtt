33d0bd290ac028c673a8346eedee9bd76115f154
Adenauer Yamin <adenauer@gmail.com>
2021-09-18 15:51:39 -0300

    Update publica-json-python.py

--- publica-json-python.py
+++ publica-json-python.py
@@ -4,7 +4,7 @@ import paho.mqtt.publish as publish
 sensor_cpu = psutil.cpu_percent(interval=1)
 
 comando = '{"id":  "cpu1", "origem":  "adenauer", "descricao":  "ocupacao cpu", "valor": ' + '"' + str(sensor_cpu)
-comando2 = comando + '"}' + "'"
+comando2 = comando + '"}'
 print(comando2)
 
 publish.single("pi/dados", comando2, hostname="broker.emqx.io")
