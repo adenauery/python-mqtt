a13b88e08c650e3af20374ac88702484164d5ccc
Adenauer Yamin <adenauer@gmail.com>
2021-09-18 10:33:41 -0300

    Update publica-json-python.py

--- publica-json-python.py
+++ publica-json-python.py
@@ -6,6 +6,5 @@ sensor_cpu = psutil.cpu_percent(interval=1)
 comando = '{"id":  "cpu1", "origem":  "adenauer", "descricao":  "ocupacao cpu", "Valor": ' + '"' + str(sensor_cpu)
 comando2 = comando + '"}' + "'"
 print(comando2)
-#os.system(comando2)
 
 publish.single("pi/dados", comando2, hostname="broker.emqx.io")
